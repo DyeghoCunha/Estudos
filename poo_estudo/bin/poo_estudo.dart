35e821af78bdb48dea444137891be17958e0b0c5
Dyegho <dyeghocunha@gmail.com>
2023-08-19 10:59:28 -0300

    docs

---
+++ poo_estudo/bin/poo_estudo.dart
@@ -0,0 +1,5 @@
+import 'package:poo_estudo/poo_estudo.dart' as poo_estudo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${poo_estudo.calculate()}!');
+}
---
+++ poo_estudo/test/poo_estudo_test.dart
@@ -0,0 +1,8 @@
+import 'package:poo_estudo/poo_estudo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
