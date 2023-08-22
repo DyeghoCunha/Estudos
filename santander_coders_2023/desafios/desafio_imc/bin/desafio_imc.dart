eb99f22b835760392f3928d41905451120d173ea
Dyegho <dyeghocunha@gmail.com>
2023-08-22 16:20:28 -0300

    Final: desafio 1 - IMC_ Santander BootCamp2023 -

--- santander_coders_2023/desafios/desafio_imc/bin/desafio_imc.dart
+++ santander_coders_2023/desafios/desafio_imc/bin/desafio_imc.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:desafio_imc/models/pessoa.dart';
 
 void main(List<String> arguments) {
-
   var nome = "";
   var peso = 0.1;
   var altura = 0.1;
@@ -13,7 +12,6 @@ void main(List<String> arguments) {
   bool validaAltura = false;
 
   var pessoa = Pessoa(nome, peso, altura);
-
   int contador = 0;
   print("Bem vindo ao Desafio 1 do Santander BootCamp 2023 da Dio!!\n");
   do {
@@ -35,48 +33,49 @@ void main(List<String> arguments) {
   print("Muito bem ${pessoa.nome}, agora me diga, qual é o seu peso ?");
 
   do {
-    var pesoEntrada = stdin.readLineSync();
-    verificaPeso = double.tryParse(pesoEntrada!);
-    if (verificaPeso == null) {
-      print("Peso inválido, digite outra vez:");
-      throw Exception("Peso invalido");
-    } else {
-      if (verificaPeso < 0) {
-        print("Peso negativo não vale, digite de novo !!");
-        throw Exception("Peso Negativo");
-      } else if (verificaPeso > 300) {
-        print("Não quero te julgar, mas eu acho que o peso não esta correto, digite novamente");
-        throw Exception("Peso muito alto");
-      } else if (verificaPeso > 0 && verificaPeso < 30) {
-        print("Eu acho que você está leve demais, digite novamente");
-        throw Exception("Peso muito baixo");
+    try {
+      var pesoEntrada = stdin.readLineSync();
+      verificaPeso = double.tryParse(pesoEntrada!);
+      if (verificaPeso == null) {
+        print("Peso inválido, digite outra vez:");
       } else {
-        validaPeso = true;
+        if (verificaPeso <= 0) {
+          print("Peso negativo não vale, digite de novo !!");
+        } else if (verificaPeso > 300) {
+          print("Não quero te julgar, mas eu acho que o peso não esta correto, digite novamente");
+        } else if (verificaPeso > 0 && verificaPeso < 30) {
+          print("Eu acho que você está leve demais, digite novamente");
+        } else {
+          validaPeso = true;
+        }
       }
+    } catch (e) {
+      print("Ocorreu um erro: $e");
     }
   } while (validaPeso == false);
 
   pessoa.peso = verificaPeso!;
   print("Legal, estamos quase lá... agora falta me falar a sua altura");
   do {
-    var alturaEntrada = stdin.readLineSync();
-    verificaAltura = double.tryParse(alturaEntrada!);
-    if (verificaAltura == null) {
-      print("Altura inválida, digite outra vez:");
-      throw Exception("Altura inválida");
-    } else {
-      if (verificaAltura < 0) {
-        print("Altura negativa não vale, digite de novo !!");
-        throw Exception("Altura negativa");
-      } else if (verificaAltura > 2.50) {
-        print("Eu acho que você não é uma girafa né, digite novamente");
-        throw Exception("Altura digitada muito alta");
-      } else if (verificaAltura > 0 && verificaAltura < 0.9) {
-        print("Eu não quero te excluir, mas eu acho que você não deve ser tão baixo assim, digita de novo:");
-        throw Exception("Altura digitada muito baixa");
+    try {
+      var alturaEntrada = stdin.readLineSync();
+      verificaAltura = double.tryParse(alturaEntrada!);
+      if (verificaAltura == null) {
+        print("Altura inválida, digite outra vez:");
       } else {
-        validaAltura = true;
+        if (verificaAltura <= 0) {
+          print("Altura negativa não vale, digite de novo !!");
+        } else if (verificaAltura > 2.50) {
+          print("Eu acho que você não é uma girafa né, digite novamente");
+        } else if (verificaAltura > 0 && verificaAltura < 0.9) {
+          print(
+              "Eu não quero te excluir, mas eu acho que você não deve ser tão baixo assim, digita de novo:");
+        } else {
+          validaAltura = true;
+        }
       }
+    } catch (e) {
+      print("Ocorreu um erro: $e");
     }
   } while (validaAltura == false);
 
