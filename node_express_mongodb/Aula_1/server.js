2deecc65dc6eef8a5782609852fa23f193fa3d5a
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2023-11-23 08:42:10 -0300

    estudo(nodeJs)

---
+++ node_express_mongodb/Aula_1/server.js
@@ -0,0 +1,18 @@
+import http from "http";
+
+const PORT = 3000;
+
+const rotas ={
+  "/": "Curso de Node.js",
+  "/livros": "Entrei na rota livros",
+  "/autores":"Entrei na rota Autores",
+}
+
+const server = http.createServer((req, res)=>{
+  res.writeHead(200,{"Content-Type":"text/plain"});
+  res.end(rotas[req.url]);
+});
+
+server.listen(PORT,()=>{
+  console.log("servidor escutando!");
+});
