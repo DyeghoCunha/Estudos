7e1ddfe7c2c3238276d2c4a273f3292dc62f0e4b
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2023-11-24 08:35:28 -0300

    estudo (express)

--- node_express_mongodb/Aula_1/server.js
+++ node_express_mongodb/Aula_1/server.js
@@ -1,18 +1,25 @@
-import http from "http";
+//import http from "http";
 
+import app from "./src/app.js";
 const PORT = 3000;
 
-const rotas ={
-  "/": "Curso de Node.js",
-  "/livros": "Entrei na rota livros",
-  "/autores":"Entrei na rota Autores",
-}
 
-const server = http.createServer((req, res)=>{
-  res.writeHead(200,{"Content-Type":"text/plain"});
-  res.end(rotas[req.url]);
-});
 
-server.listen(PORT,()=>{
+
+
+//? Parte do código que fui incluida quando não usavamos o EXPRESS
+// const rotas ={
+//   "/": "Curso de Node.js",
+//   "/livros": "Entrei na rota livros",
+//   "/autores":"Entrei na rota Autores",
+// }
+
+// const server = http.createServer((req, res)=>{
+//   res.writeHead(200,{"Content-Type":"text/plain"});
+//   res.end(rotas[req.url]);
+// });
+//? ------------
+
+app.listen(PORT,()=>{
   console.log("servidor escutando!");
 });
