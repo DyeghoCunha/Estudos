fe124d41c9443585fd40499628e426bf68f02af1
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2024-01-24 12:44:21 -0300

    estudo

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -5,5 +5,5 @@ const controller = new NegociacaoController();
 const form = document.querySelector(".form");
 form.addEventListener("submit", event => {
   event.preventDefault();
-  controller.adiciona;
+  controller.adiciona();
 })
--- typescript-curso-1-arquivos-iniciais/dist/js/app.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/app.js
@@ -3,5 +3,5 @@ const controller = new NegociacaoController();
 const form = document.querySelector(".form");
 form.addEventListener("submit", event => {
     event.preventDefault();
-    controller.adiciona;
+    controller.adiciona();
 });
