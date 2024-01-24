41732dc61860a797302f0ce76971a7ffa43b561d
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2024-01-24 16:26:39 -0300

    estudo

--- typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,28 +1,18 @@
 export class Negociacao {
 
-  private _data: Date;
-  private _quantidade: number;
-  private _valor: number;
+  constructor(
+    private _data: Date,
+    public readonly quantidade: number,
+    public readonly valor: number) {}
 
-  constructor(data: Date, quantidade: number, valor: number) {
-    this._data = data;
-    this._quantidade = quantidade;
-    this._valor = valor;
-  }
+    get data():Date{
+      const data = new Date(this._data.getTime());
+      return data;
+    }
 
-  get data():Date {
-    return this._data;
-  }
-
-  get quantidade():number {
-    return this._quantidade;
-  }
-
-  get valor():number {
-    return this._valor;
-  }
-
-  get volume():number {
-    return this._quantidade * this._valor;
+  get volume(): number {
+    return this.quantidade * this.valor;
   }
 }
+
+//!! O CODIGO FOI ALTERADO PARA TIRAR O PRIVATE E OS GETTERS, COM OS PUBLIC READONLY VOCÊ PODE TIRAR OS _ E GETTERS
