47e9ab779fe3dcfcae563e078b2a2de264986e0c
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2024-02-16 15:30:21 -0300

    finalizando ( pré build)

--- NEXT/Udemy/Maximillian_Next_14/05-onwards-foodies-starting-project/app/meals/share/page.js
+++ NEXT/Udemy/Maximillian_Next_14/05-onwards-foodies-starting-project/app/meals/share/page.js
@@ -1,13 +1,13 @@
 "use client";
-import {useFormStatus} from "react-dom";
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/action';
+import MealsFormSubmit from '../meals-form-submit';
+import {useFormState} from "react-dom"
 
 export default function ShareMealPage() {
- const status = useFormStatus();
-
-
+ const [state, formAction] = useFormState(shareMeal,{message:null})
 
   return (
     <>
@@ -18,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -47,8 +47,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your Image" name="image" />
+         {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit/>
           </p>
         </form>
       </main>
